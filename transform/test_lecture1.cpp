ed72afd6db748335e763bae8475ab52735493b21
iseine <2509895377@qq.com>
2026-02-09 23:05:52 +0800

    修改了平移的计算方法，平移矩阵，final中不正确的元素值计算，增加了每一步的测试

--- transform/test_lecture1.cpp
+++ transform/test_lecture1.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-struct Matrix2D{
-	double matrix[3][3] = {0};
+struct Matrix2D {
+	double matrix[3][3] = { 0 };
 };
 
 
@@ -36,27 +36,40 @@ struct Matrix2D muilti(struct Matrix2D mat1, struct Matrix2D mat2)
 				sum.matrix[i][j] += mat1.matrix[i][k] * mat2.matrix[k][j];
 			}
 		}
-		
+
 	}
 	return sum;
 }
 
-void fina(struct Matrix2D mat,double point[3]) 
+struct Matrix2D add(struct Matrix2D mat1, struct Matrix2D mat2)
 {
-	double finally[3] = {0};
+	struct Matrix2D sum;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			sum.matrix[i][j] = mat1.matrix[i][j] + mat2.matrix[i][j];
+		}
+	}
+	return sum;
+}
+
+void fina(struct Matrix2D mat, double point[3])
+{
+	double finally[3] = { 0,0,0 };
 	std::cout << "(";
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			finally[j]+= point[i] * mat.matrix[i][j];
+			finally[i] += point[j] * mat.matrix[i][j];
 		}
 	}
 	std::cout << finally[0];
 	std::cout << ",";
 	std::cout << finally[1];
-	std::cout << ")";
-	
+	std::cout << ")" << std::endl;
+
 }
 void test_matrix_multi() {
 	double A_data[9] = { 1, 0, 0,
@@ -66,16 +79,22 @@ void test_matrix_multi() {
 						0, 2, 0,
 						0, 0, 1 }; // 缩放2倍
 
-	Matrix2D A, B, C;
+	Matrix2D A, B, C, D;
 	makematrix(A_data, &A);
 	makematrix(B_data, &B);
 
 	C = muilti(A, B); // 单位矩阵乘任何矩阵应该等于原矩阵
+	D = add(A, B);
 
 	std::cout << "测试矩阵乘法 - 单位矩阵测试：" << std::endl;
 	std::cout << "C[0][0]应该是2，实际是：" << C.matrix[0][0] << std::endl;
 	std::cout << "C[1][1]应该是2，实际是：" << C.matrix[1][1] << std::endl;
 	std::cout << "C[2][2]应该是1，实际是：" << C.matrix[2][2] << std::endl;
+	std::cout << std::endl;
+	std::cout << "测试矩阵加法 - 单位矩阵测试：" << std::endl;
+	std::cout << "C[0][0]应该是3，实际是：" << D.matrix[0][0] << std::endl;
+	std::cout << "C[1][0]应该是0，实际是：" << D.matrix[1][0] << std::endl;
+	std::cout << "C[2][2]应该是2，实际是：" << D.matrix[2][2] << std::endl;
 }
 int main()
 {
@@ -84,28 +103,34 @@ int main()
 	double degrees = 30.0;
 	double rad = degrees * PI / 180;
 
-	std::cout << "点（3，4）绕原点旋转30度后缩放2倍再向右平移1是：" << endl;
+	std::cout << "点(2,0）绕原点旋转30度后缩放2倍再向右平移1是：" << endl;
 
 
-	double point[3] = { 3,4,1 };
+	double point[3] = { 2,0,1 };
 
 	double rotate[9] = { cos(rad),-sin(rad),0,
 						 sin(rad),cos(rad),0,
-						   0,0,1
+						   0,0,0
 	};
 	double scale[9] = { 2,0,0,
 						0,2,0,
-						0,0,1 };
-	double trans[9] = { 1,0,1,
-						0,1,0,
+						0,0,0 };
+	double trans[9] = { 0,0,1,
+						0,0,0,
 						0,0,1 };
 
-	struct Matrix2D rot, sca, tran;
+	Matrix2D rot, sca, tran;
 	makematrix(rotate, &rot);
 	makematrix(scale, &sca);
 	makematrix(trans, &tran);
+	std::cout << "经过旋转后的点：" << std::endl;
+	fina(rot, point);
+	std::cout << "经过缩放后的点：" << std::endl;
+	fina(sca, point);
+	std::cout << "经过旋转和缩放后的点：" << std::endl;
+	fina(muilti(rot, sca), point);
+	std::cout << "经过旋转缩放和平移后的点：" << std::endl;
+	fina(add(muilti(rot, sca), tran), point);
+	getchar();
 
-	fina(muilti(muilti(rot, sca), tran), point);
-
-	
 }
